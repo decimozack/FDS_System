eeaa7efed2fc05fc9dd9db17cbd3e5f878f8231b
Zack Chua Zhi Jie <zackwco9@gmail.com>
2020-03-23 20:27:49 +0800

    setup basic frame

--- frontend/fds-app/src/App.js
+++ frontend/fds-app/src/App.js
@@ -1,32 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import SignIn from "./components/Signin";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3)
+  }
+}));
+
+const sections = [
+  { title: "Home", url: "/" },
+  { title: "Contact", url: "/contact" },
+  { title: "About", url: "/about" }
+];
 
 const routing = (
-  <BrowserRouter>
-    <div>
-      <h3>FDS System</h3>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+  <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      <Header title="FDS System" sections={sections} />
+      <main>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </main>
+    </Container>
+  </React.Fragment>
 );
 
 class App extends Component {
