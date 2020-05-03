2fcb0e271e0beed36583bd46e86b319fabda58a1
Zack Chua Zhi Jie <zackwco9@gmail.com>
2020-05-03 19:53:05 +0800

    clean up customer trigger

--- database/customer_triggers.sql
+++ database/customer_triggers.sql
@@ -1,34 +1,4 @@
 
-drop type test_type CASCADE;
-create type test_type as (a Text, b int);
-
--- Check existing email for user creation
-drop function testing(text) CASCADE;
-CREATE OR REPLACE FUNCTION testing(inJson text)
--- RETURNS setof test_type as $$
-RETURNS text as $$
-DECLARE
-    outTest text;
-BEGIN
-    RAISE NOTICE '%', inJson;
-    RAISE exception '% is already existing in the login table', 'test';
-    select a into outTest from json_populate_recordset(null:: test_type, inJson::json);
-    return a;
-    -- return query select * from json_populate_recordset(null:: test_type, inJson::json);
-    -- return outTest;
-END;
-$$ LANGUAGE plpgsql;
-
-drop function testingSecond(text) CASCADE;
-CREATE OR REPLACE FUNCTION testingSecond(inJson text)
-RETURNS setof test_type as $$
-BEGIN
-     RAISE NOTICE '%', inJson;
-    return query select * from json_populate_recordset(null:: test_type, inJson::json);
-END;
-$$ LANGUAGE plpgsql;
-
-
 DROP TYPE OrderItemInput CASCADE;
 CREATE TYPE OrderItemInput as (catid integer, catname text,
 current_qty integer, description text, fid integer,
@@ -73,18 +43,6 @@ BEGIN
     
 END;
 $$ LANGUAGE plpgsql;
-
--- drop function createOrder(text) CASCADE;
--- CREATE OR REPLACE FUNCTION testing(inJson text)
--- RETURNS setof test_type as $$
--- DECLARE
---     outTest text;
--- BEGIN
---     RAISE NOTICE '%', inJson;
---     return query select * from json_populate_recordset(null:: test_type, inJson::json);
---     -- return outTest;
--- END;
--- $$ LANGUAGE plpgsql;
 
 -- Customer triggers and functions -------
 
@@ -151,6 +109,7 @@ ON OrderItem
 DEFERRABLE INITIALLY IMMEDIATE
 FOR EACH ROW
 EXECUTE FUNCTION create_orderitem_task();
+
 
 
 
